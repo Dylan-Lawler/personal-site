be90ece3d6c0807198ec454e4e4c99ed0ebd596c
Dylan-Lawler <dylan.g.lawler.24@dartmouth.edu>
2023-05-13 18:30:28 -0400

    first attempt

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Resume from './Resume';
+import './App.css'; // Import the CSS file for styling
+import profilePhoto from './profile.jpg';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <img
+        src={profilePhoto}
+        alt="Your Name"
+        className="profile-photo"
+      />
+      <div className="resume-container">
+        <Resume />
+      </div>
     </div>
   );
 }
---
+++ src/Resume.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const Resume = () => {
+  return (
+    <div>
+      <header>
+        <h1>Dylan Lawler</h1>
+        <h2>Computer Engineering Student</h2>
+      </header>
+      <section>
+        <h3>About Me</h3>
+        <p>
+          I am a student at Dartmouth College passionate about computer engineering with over a year experience in each C, Python, Java, and Linux with recent experience in JavaScript, HTML, CSS, and ReactJS.  
+        </p>
+        <h3>Education</h3>
+        <p>
+         Dartmouth College 
+        </p>
+        <p>
+         Dutchess Community College 
+        </p>
+      </section>
+
+       
+      <footer>
+        <p>&copy; 2023 Dylan Lawler</p>
+      </footer>
+    </div>
+  );
+};
+
+export default Resume;
